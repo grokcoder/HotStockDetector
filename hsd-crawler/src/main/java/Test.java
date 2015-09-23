19d8d0de21e7a14b7cc326e75c8a91b8bfa9f1cd
汪小益 <eatvlis@gmail.com>
2015-09-23 11:43:35 +0800

    init the base structure of th project

---
+++ hsd-crawler/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by wangxiaoyi on 15/9/23.
+ */
+public class Test {
+
+
+    public static void main(String []args){
+
+    }
+
+
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import org.apache.logging.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Created by wangxiaoyi on 15/9/23.
+ */
+public class Test {
+
+    public static void main(String []args){
+        Logger logger = LogManager.getLogger(Test.class.getName());
+        System.out.println("hello");
+        logger.error("ssss");
+    }
+}
