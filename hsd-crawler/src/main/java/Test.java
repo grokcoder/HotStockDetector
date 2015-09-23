a138cb5bd7829293cf8f08c64b884184a67e30e0
汪小益 <eatvlis@gmail.com>
2015-09-23 19:24:30 +0800

    add configuration, log and junit module

--- hsd-crawler/src/main/java/Test.java
+++ hsd-crawler/src/main/java/Test.java
@@ -1,3 +1,10 @@
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
 /**
  * Created by wangxiaoyi on 15/9/23.
  */
@@ -5,6 +12,13 @@ public class Test {
 
 
     public static void main(String []args){
+        Logger logger = LogManager.getLogger(Test.class.getName());
+        logger.info("Test");
+
+
+        Config conf = ConfigFactory.load();
+        logger.info(conf.getString("b.hbase.retru"));
+
 
     }
 
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.LogManager;
 public class Test {
 
     public static void main(String []args){
+
         Logger logger = LogManager.getLogger(Test.class.getName());
         System.out.println("hello");
         logger.error("ssss");
