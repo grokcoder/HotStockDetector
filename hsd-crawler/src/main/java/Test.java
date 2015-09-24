84a48fbf1beaf0cf8c38efd08b733061f2ca3ec7
汪小益 <eatvlis@gmail.com>
2015-09-24 09:43:37 +0800

    add modules : hsd-common hsd-assembly hsd-analysis

--- hsd-crawler/src/main/java/Test.java
+++ hsd-crawler/src/main/java/Test.java
@@ -1,8 +1,3 @@
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 
 
 /**
@@ -12,13 +7,6 @@ public class Test {
 
 
     public static void main(String []args){
-        Logger logger = LogManager.getLogger(Test.class.getName());
-        logger.info("Test");
-
-
-        Config conf = ConfigFactory.load();
-        logger.info(conf.getString("b.hbase.retru"));
-
 
     }
 
